2275dbb97a43bfd005b0f128a014b82f96be122a
Rittik123-eng <83904674+Rittik123-eng@users.noreply.github.com>
2021-05-11 07:15:52 -0700

    Add files via upload

---
+++ problem 3...multiply of two no.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+int a,b,mult;
+printf("Enter the two no");
+scanf("%d %d",&a,&b);
+
+mult=a*b;
+{
+    printf("multiple of both no is %d",mult);
+}
+}
