8106fee6e480fd980aa1fc7f5b7928565d0e912a
Rittik123-eng <83904674+Rittik123-eng@users.noreply.github.com>
2021-05-17 07:37:07 -0700

    Add files via upload

---
+++ LAB 2/17th may..ones digit.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    printf("Enter the no");
+    scanf("%d",&x);
+
+    y=x%10;
+    printf("Digit at ones place=%d",y);
+    return 0;
+}
